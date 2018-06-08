7c504445c6736c8cdef73e79f00d35c572cfea86
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-06-08 16:43:25 +0100

    Fixed deprecated warning. Fixing unit tests

--- test/unit/multiplexed_library_creation_pipeline_test.rb
+++ test/unit/multiplexed_library_creation_pipeline_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 
 class MultiplexedLibraryCreationPipelineTest < ActiveSupport::TestCase
   def setup
-    @pipeline = Pipeline.find_by(name: 'Illumina-B MX Library Preparation') or raise StandardError, 'Cannot find the Illumina-B MX Library Preparation pipeline'
+    # @pipeline = Pipeline.find_by(name: 'Illumina-B MX Library Preparation') or raise StandardError, 'Cannot find the Illumina-B MX Library Preparation pipeline'
+    @pipeline = create(:multiplexed_pipeline)
     @user     = create(:user)
   end
 
